2de06c7d1276b37d7ac42d57543b6ac9e86075d5
samanthaduarte <samanthamdc97@gmail.com>
2018-11-05 19:50:51 -0600

    validadores de campos

--- pupille-app/src/components/SignInModal/index.jsx
+++ pupille-app/src/components/SignInModal/index.jsx
@@ -4,6 +4,14 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import React from 'react'
 
+const required = value => value ? undefined : 'Este campo es obligatorio'
+const email = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
+  'Dirección de correo inválida' : undefined
+const password = value =>
+  value && !/^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{6}$/i.test(value) ?
+    'Contraseña insegura' : undefined
+    
 
 const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
     <div>
@@ -21,12 +29,15 @@ const SignInForm = (props) => {
     <form onSubmit={handleSubmit}>
       <Field name="username" type="text"
         component={renderField} label="Username"
+        validate={required}
       />
       <Field name="email" type="email"
         component={renderField} label="Email"
+        validate={email}
       />
       <Field name="pw" type="password"
         component={renderField} label="Password"
+        validate={[required,password]}
       />
       <div>
         <button type="submit" disabled={submitting}>Crear</button>
